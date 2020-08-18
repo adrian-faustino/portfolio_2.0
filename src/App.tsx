99bf26c9694a64c766d40ccd819503fc4ec58993
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-18 13:31:41 -0400

    deploying

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      test
+      <h3>test</h3>
+    </div>
+  );
 }
 
 export default App;
