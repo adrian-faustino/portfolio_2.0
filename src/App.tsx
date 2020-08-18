bc7d2dc666d57fdd18811084e780f2348b634829
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-18 13:34:31 -0400

    deploying

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       test
-      <h3>test</h3>
+      <h3>tes1t</h3>
     </div>
   );
 }
