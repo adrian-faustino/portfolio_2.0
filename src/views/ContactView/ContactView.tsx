e46253e0a761f099041cd9cc819074b330465c51
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-20 13:45:12 -0400

    Added icons and displaying contact info

--- src/views/ContactView/ContactView.tsx
+++ src/views/ContactView/ContactView.tsx
@@ -5,18 +5,22 @@ import { LOCATION, EMAIL, MOBILE_NUMBER } from "../../db/myAboutMe";
 const ContactView = () => {
   return (
     <div>
+      <h2>Contact</h2>
       {/* location */}
       <div>
+        <img src="portfolio_2.0_assets/icons/location.png" />
         <span>{LOCATION}</span>
       </div>
 
       {/* email */}
       <div>
+        <img src="portfolio_2.0_assets/icons/email.png" />
         <span>{EMAIL}</span>
       </div>
 
       {/* mobile number */}
       <div>
+        <img src="portfolio_2.0_assets/icons/mobile_phone.png" />
         <span>{MOBILE_NUMBER}</span>
       </div>
     </div>
