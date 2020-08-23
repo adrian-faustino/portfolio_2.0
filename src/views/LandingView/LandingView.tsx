070af1b8f55e8f5e3acbedb8467aa165600e5817
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-23 13:39:37 -0400

    adding focus view btn on landing view

--- src/views/LandingView/LandingView.tsx
+++ src/views/LandingView/LandingView.tsx
@@ -5,11 +5,13 @@ import "./LandingView.css";
 const LandingView = () => {
   return (
     <div className="LandingView__main-container">
-      <img
-        className="LandingView__logo"
-        src="/portfolio_2.0_assets/images/myLogo.png"
-        alt="Adrian Faustino logo"
-      />
+      <div className="LandingView__logo-container">
+        <img
+          className="LandingView__logo"
+          src="/portfolio_2.0_assets/images/myLogo.png"
+          alt="Adrian Faustino logo"
+        />
+      </div>
     </div>
   );
 };
