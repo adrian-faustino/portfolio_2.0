dd791d16fa468f7b8df588bf012a389c837eea0b
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-23 12:56:36 -0400

    adding landing page and logo

---
+++ src/views/LandingView/LandingView.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+/* Styles */
+import "./LandingView.css";
+
+const LandingView = () => {
+  return (
+    <div className="LandingView__main-container">
+      <img
+        className="LandingView__logo"
+        src="/portfolio_2.0_assets/images/myLogo.png"
+        alt="Adrian Faustino logo"
+      />
+    </div>
+  );
+};
+
+export default LandingView;
--- src/views/index.ts
+++ src/views/index.ts
@@ -2,3 +2,4 @@ export { default as HomeView } from "./HomeView/HomeView";
 export { default as ProjectsView } from "./ProjectsView/ProjectsView";
 export { default as SkillsView } from "./SkillsView/SkillsView";
 export { default as ContactView } from "./ContactView/ContactView";
+export { default as LandingView } from "./LandingView/LandingView";
