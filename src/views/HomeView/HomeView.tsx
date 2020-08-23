128f587aeb1349243aa3fa8122694a76c33bd467
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-23 04:01:00 -0400

    deploying

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -15,18 +15,28 @@ import { ExternalLinks, CurrentStatus } from "../../components";
 const HomeView = () => {
   return (
     <section className="HomeView__">
+      {/* name */}
       <h1 className="HomeVew__full-name">{FULL_NAME}</h1>
+
+      {/* job title */}
       <h2 className="HomeView__description">{JOB_DESCRIPTION}</h2>
+
+      {/* avatar */}
       <div className="HomeView__avatar-container">
         <img className="HomeView__avatar" src={AVATAR_PATH} alt="my avatar" />
       </div>
+
+      {/* about me paragraphs */}
       <div className="HomeView__about-me">
         <p>{ABOUT_ME_P1}</p>
+        <br />
         <p>{ABOUT_ME_P2}</p>
       </div>
 
+      {/* link to github, resume etc */}
       <ExternalLinks />
 
+      {/* current employer, learning, etc */}
       <h2 className="section-title">My Status</h2>
       <CurrentStatus />
     </section>
