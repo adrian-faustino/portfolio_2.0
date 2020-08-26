fcdb53fc18e969d09f253ace4f97100a249c35d5
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-26 04:16:56 -0400

    modularized mobile nav to new folder

---
+++ src/components/MobileNav/MobileNav.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+/* Material UI */
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+/* Constants */
+import { SECTIONS } from "../../constants/skeleton";
+
+const MobileNav = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [selectedIndex, setSelectedIndex] = useState(1);
+
+  const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuItemClick = (
+    event: React.MouseEvent<HTMLElement>,
+    index: number
+  ) => {
+    setSelectedIndex(index);
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <List component="nav" aria-label="Device settings">
+        <ListItem
+          button
+          aria-haspopup="true"
+          aria-controls="lock-menu"
+          aria-label="when device is locked"
+          onClick={handleClickListItem}
+        >
+          <ListItemText primary={SECTIONS[selectedIndex]} />
+        </ListItem>
+      </List>
+      <Menu
+        id="lock-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {SECTIONS.map((option, index) => (
+          <MenuItem
+            key={option.toString()}
+            selected={index === selectedIndex}
+            onClick={(event) => handleMenuItemClick(event, index)}
+          >
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+};
+
+export default MobileNav;
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -8,12 +8,8 @@ import Link from "@material-ui/core/Link";
 import { SECTIONS, viewIDs } from "../../constants/skeleton";
 /* Util */
 import { scrollToRef } from "../../util";
-
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
+/* Subcomponents */
+import { MobileNav } from "../";
 
 interface NavProps {
   homeViewRef: React.RefObject<HTMLDivElement>;
@@ -42,25 +38,6 @@ const Nav: React.FC<NavProps> = (props) => {
     }
   };
 
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
-
-  const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuItemClick = (
-    event: React.MouseEvent<HTMLElement>,
-    index: number
-  ) => {
-    setSelectedIndex(index);
-    setAnchorEl(null);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   const spreadNavBreadCrumbsJSX = () => {
     return SECTIONS.map((sectionName, i) => (
       <Link
@@ -84,34 +61,7 @@ const Nav: React.FC<NavProps> = (props) => {
       </div>
 
       <div className="Nav__mobile hide">
-        <List component="nav" aria-label="Device settings">
-          <ListItem
-            button
-            aria-haspopup="true"
-            aria-controls="lock-menu"
-            aria-label="when device is locked"
-            onClick={handleClickListItem}
-          >
-            <ListItemText primary={SECTIONS[selectedIndex]} />
-          </ListItem>
-        </List>
-        <Menu
-          id="lock-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          {SECTIONS.map((option, index) => (
-            <MenuItem
-              key={option.toString()}
-              selected={index === selectedIndex}
-              onClick={(event) => handleMenuItemClick(event, index)}
-            >
-              {option}
-            </MenuItem>
-          ))}
-        </Menu>
+        <MobileNav />
       </div>
     </nav>
   );
