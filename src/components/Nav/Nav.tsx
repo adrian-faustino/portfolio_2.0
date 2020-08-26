516086f5b393a36006ea0ddcd0897727429b9d7f
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-26 03:58:06 -0400

    Adding mobile nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -9,6 +9,12 @@ import { SECTIONS, viewIDs } from "../../constants/skeleton";
 /* Util */
 import { scrollToRef } from "../../util";
 
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+
 interface NavProps {
   homeViewRef: React.RefObject<HTMLDivElement>;
   projectsViewRef: React.RefObject<HTMLDivElement>;
@@ -36,6 +42,25 @@ const Nav: React.FC<NavProps> = (props) => {
     }
   };
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuItemClick = (
+    event: React.MouseEvent<HTMLElement>,
+    index: number
+  ) => {
+    setSelectedIndex(index);
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const spreadNavBreadCrumbsJSX = () => {
     return SECTIONS.map((sectionName, i) => (
       <Link
@@ -52,9 +77,42 @@ const Nav: React.FC<NavProps> = (props) => {
 
   return (
     <nav className="Nav__main-container">
-      <Breadcrumbs aria-label="breadcrumb">
-        {spreadNavBreadCrumbsJSX()}
-      </Breadcrumbs>
+      <div className="Nav__desktop hide">
+        <Breadcrumbs aria-label="breadcrumb">
+          {spreadNavBreadCrumbsJSX()}
+        </Breadcrumbs>
+      </div>
+
+      <div className="Nav__mobile hide">
+        <List component="nav" aria-label="Device settings">
+          <ListItem
+            button
+            aria-haspopup="true"
+            aria-controls="lock-menu"
+            aria-label="when device is locked"
+            onClick={handleClickListItem}
+          >
+            <ListItemText primary={SECTIONS[selectedIndex]} />
+          </ListItem>
+        </List>
+        <Menu
+          id="lock-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          {SECTIONS.map((option, index) => (
+            <MenuItem
+              key={option.toString()}
+              selected={index === selectedIndex}
+              onClick={(event) => handleMenuItemClick(event, index)}
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
+      </div>
     </nav>
   );
 };
