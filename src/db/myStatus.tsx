a8a4f1d609a4dc41f666d40b87e75831f77c1254
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-10-02 02:08:57 -0400

    Update status and resume

--- src/db/myStatus.tsx
+++ src/db/myStatus.tsx
@@ -1,7 +1,9 @@
 // These are displayed on the landing page HomeView.tsx
-export const EMPLOYMENT_STATUS: Array<String> = ["Actively Searching"];
+export const EMPLOYMENT_STATUS: Array<String> = [
+  "Searching for long term employment",
+];
 
-export const CURRENTLY_LEARNING: Array<String> = ["PixiJS"];
+export const CURRENTLY_LEARNING: Array<String> = ["RiotJS", "Python"];
 
 export const CURRENTLY_REVIEWING: Array<String> = [
   "MVC Model",
@@ -9,4 +11,8 @@ export const CURRENTLY_REVIEWING: Array<String> = [
   "OOP",
 ];
 
-export const LEARNING_NEXT: Array<String> = ["React Native", "BEM", "GraphQL"];
+export const LEARNING_NEXT: Array<String> = [
+  "React Native",
+  "Django",
+  "GraphQL",
+];
